6bd0213a96473e9e5c45004f09568b8a21e63253
jabet01 <aliefimran662014@gmail.com>
2025-06-10 20:47:33 +0800

    learn basic js

---
+++ session3/basic_script.js
@@ -0,0 +1,39 @@
+// console.log(5)
+
+let x = 5
+console.log(x)
+
+x = 7
+console.log(x)
+
+const y = 8
+console.log(y)
+
+
+console.log((7 + 3) * 10)
+
+let seven = 7
+let three = 3
+console.log((seven + three) * 10)
+
+let banana = "hello"
+let mango = " durian"
+console.log(banana + mango)
+
+
+
+let one = "1"
+let two = "2"
+console.log(one + two)
+
+let nm = 3.1412
+console.log(typeof nm)
+let myBool = true
+myBool = 3
+console.log(typeof myBool)
+
+console.log("-----------------------")
+
+let anArray = [213, 43, 908, 3, 34, undefined, null, true, false, "Hello", [1,2,3]]
+console.log("lengthof array: ", anArray.length)
+console.log("lengthof array: ", anArray.length)
